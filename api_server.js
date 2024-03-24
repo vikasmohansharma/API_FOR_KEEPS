d4da6f48941e5300fc1d7bb339611d3583ad12fc
Your Name <you@example.com>
2024-03-24 09:15:39 +0530

    Commit 26

--- api_server.js
+++ api_server.js
@@ -55,7 +55,7 @@ app.post('/login', passport.authenticate('local'), (req, res) => {
 
 
 //Route to check deployement
-app.get('/api', (req,res) => {
+app.get('/', (req,res) => {
   res.send("Hello World");
 })
 
