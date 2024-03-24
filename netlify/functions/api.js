87f0553f73f3e2bf30b574caeecfe014dd1a76b2
Your Name <you@example.com>
2024-03-24 09:27:01 +0530

    Commit 27

--- netlify/functions/api.js
+++ netlify/functions/api.js
@@ -9,4 +9,22 @@ router.get("/hello", (req, res) => res.send("Hello World!"));
 
 api.use("/api/", router);
 
+app.get('/notes/show/:id', async (req, res) => {
+    try {
+      var user = parseInt(req.params.id);
+      //console.log(`user = ${user}`);
+      const result = await supabase
+      .from('notes_table')
+      .select('*')
+      .eq('user_id', user)
+      .order('note_id')
+    
+      //console.log(result);
+      res.json(result.data);
+    } catch (error) {
+      console.error('Error fetching notes:', error);
+      res.status(500).json({ error: 'Error fetching notes' });
+    }
+  });
+
 export const handler = serverless(api);
